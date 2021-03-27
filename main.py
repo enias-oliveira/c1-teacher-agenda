2d08920da025b130b5b5d50984144ba318d64739
Enias Oliveira <eniasoliveira27@gmail.com>
2021-03-26 21:43:59 -0300

    feat: adds make_appoint route

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 
 def create_app():
     app = Flask(__name__)
+
+    @app.route("/appointment", methods=["POST"])
+    def make_appointment():
+        appointment = request.get_json()
+
+        from services.save_to_csv import create_appointment
+
+        service_response = create_appointment(appointment)
+
+        if not service_response:
+            return {}
+
+        return service_response
 
     return app
